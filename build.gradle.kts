36b7b80e1e28bae80534bbb10e86ebcd8f901bcd
Minenhle Sithole <minenhle@skadush.co.za>
2026-04-16 21:26:45 +0200

    fix java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,11 @@
 plugins {
 	kotlin("jvm") version "2.3.0"
 }
+
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension> {
+            jvmToolchain(21)
+        }
+    }
+}
